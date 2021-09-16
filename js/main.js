3cceb9068cf003c632260bf8002a07ba46374548
Seunghyeon Yeom <ysh.hyeon@gmail.com>
2021-09-15 22:44:16 -0400

    coding js

--- js/main.js
+++ js/main.js
@@ -7,18 +7,6 @@
         const nav = document.querySelector(".nav-links");
         const navLinks = document.querySelectorAll(".nav-links li");
 
-        const slideImageBox = document.querySelectorAll(".portfolioShowing");
-        const slideContainer = document.querySelector(".mySlides");
-        const prevBtn = document.querySelector(".previous");
-        const nextBtn = document.querySelector(".next");
-        const navDots = document.querySelector(".navigation-dots");
-
-        let numberOfImages = slideImageBox.length;
-        let slideWidth = slideImageBox[0].clientWidth;
-        let currentSlide = 0;
-        
-
-
         // Burger Nav
 
         burger.addEventListener("click", () => {
@@ -36,66 +24,6 @@
             //Burger Animation
             burger.classList.toggle("toggle");
         });
-
-
-
-
-
-        //Set Up The Slider
-
-        /*
-        
-        slideImageBox[1] = 1380
-        slideImageBox[2] = 2760
-        
-        */
-        
-        function init() {
-            slideImageBox.forEach((img, i) => {
-                img.style.left = i * 1380 + "px";
-            });
-
-            slideImageBox[0].classList.add("active");
-
-            navigationDots();
-        }
-
-        init();
-
-        // Navigation Dots
-
-        function navigationDots() {
-            for (let i = 0; i < numberOfImages; i++){
-                const dot = document.createElement("div");
-                dot.classList.add("single-dot");
-                navDots.appendChild(dot);
-            }
-
-            navDots.children[0].classList.add("active");
-        }
-
-        // function animateBanner(event) {
-        //     if (event.target.className.includes('portfolioShowing')) {
-        //         let multiplier = event.target.dataset.offset,
-        //             offsetWidth = 1380;
-    
-        //         // 0 ,600, 1200,1800px depending on the math
-        //         slideImageBox.style.right = `${multiplier * offsetWidth}px`;
-        //     }
-        // }
-
-        //Buttons
-
-        nextBtn.addEventListener ("click", () => {
-            currentSlide++;
-            goToSlide(currentSlide);
-        });
-
-        function goToSlide(slideNumber) {
-            slideContainer.style.transform = "translateX(-" + slideWidth * slideNumber + "px)";
-        };
-
-        // nextBtn.addEventListener("click", animateBanner);
 
     })();
 
